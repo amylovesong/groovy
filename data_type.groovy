821eb8b2c5f1283cd55f99c3a7a7d9c2f1424eb1
sunxiaoling <sunxiaoling@didichuxing.com>
2016-04-27 19:34:00 +0800

    Range

--- data_type.groovy
+++ data_type.groovy
@@ -1,6 +1,8 @@
+println "Primary datatype example: "
 def int x = 1
 println x.getClass().getCanonicalName()
 
+println "\nList example:"
 def aList = [5, 'string', true]
 assert aList[1] == 'string'
 assert aList[5] == null
@@ -8,8 +10,15 @@ aList[100] = 100
 assert aList[100] == 100
 println "aList.size: " + aList.size
 
+println "\nMap example:"
 def aMap = ['key1':'value1', 'key2':true]
 println "aMap.key1: " + aMap.key1
 println "aMap['key2']: " + aMap['key2']
 aMap.anotherkey = "I am map"
 println "aMap: " + aMap
+
+println "\nRange example:"
+def aRange = 1..5
+def aRangeWithoutEnd = 1..<5
+println "aRange: " + aRange
+println "aRangeWithoutEnd: " + aRangeWithoutEnd
