74bb54b77bd633ed154058d0e1696b8135fab59d
sunxiaoling <sunxiaoling@didichuxing.com>
2016-04-27 18:45:17 +0800

    List and Map

--- data_type.groovy
+++ data_type.groovy
@@ -1,2 +1,15 @@
 def int x = 1
 println x.getClass().getCanonicalName()
+
+def aList = [5, 'string', true]
+assert aList[1] == 'string'
+assert aList[5] == null
+aList[100] = 100
+assert aList[100] == 100
+println "aList.size: " + aList.size
+
+def aMap = ['key1':'value1', 'key2':true]
+println "aMap.key1: " + aMap.key1
+println "aMap['key2']: " + aMap['key2']
+aMap.anotherkey = "I am map"
+println "aMap: " + aMap
